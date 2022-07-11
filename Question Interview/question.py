3455e8d830c4d6026c81edb69ddaa9bbde9713d5
Tontan-Hak <tontanhak97@gmail.com>
2022-07-11 17:21:25 +0700

    Add questions

--- Question Interview/question.py
+++ Question Interview/question.py
@@ -1,16 +1,41 @@
 # 1 Write a python program to print Prime Numbers between 2 numbers
-for num in range (100,200):
-    if all(num%i!=0 for i in range (2,num)):
-        print(num)
+# for num in range (100,200):
+#     if all(num%i!=0 for i in range (2,num)):
+#         print(num)
 
 # 1a Write python program to print how many Prime numbers between 2 numbers
-amount = 0 
-for num in range (100,201):
-    for i in range (2, 100):
-        if (num%i==0):
-            break
-        else:
-            amount = amount + 1
-print(amount)
+# amount = 0 
+# for num in range (100,201):
+#     for i in range (2, 100):
+#         if (num%i==0):
+#             break
+#         else:
+#             amount = amount + 1
+# print(amount)
+# amount = 0 
+# prime = True
+# for num in range (100,201):
+#     for i in range (2, 100):
+#         if (num%i==0):
+#             break
+#         else:
+#             amount = amount + 1
+# print(amount)
+# 2.  Write a sort function to sort the elements in a list 
+for i in [0,2,1,4,3,5,7,6,8,9]:
+    print(i)
 
-# 2  
+# 3. Write a sorting function without using the list.sort function 
+
+# 4. Write a python program to print a Fibonacci Series
+# 5. Write a Python program to prrint a list in reverse 
+# 6. Write a python program to check whether a string is a Palindrome or not
+# 7. Write a python program to print set of duplicates in a list
+# 8. Write a python program to print number of words in a given sentence
+# 9. Write an array arr[] of n elements, write a python function to search a given element x in  arr[].
+# 10. Write a python program to impliment a Binary search 
+# 11. Write a python program to plot a simple bar chart 
+# 12. Write a python program to joun two strings (Hint: using join())
+# 13. Write a python program to extract digits from given string 
+# 14. Write a python program to split strings using newline delimiter 
+# 15. Given a string as your imput, delete any reoccurring character, and return the new string.
