3c7dce9c8e7ae3f6bfeb21b24ec9bfd548ed103a
Tontan-Hak <tontanhak97@gmail.com>
2022-07-13 17:09:35 +0700

    add new question answer

--- Question Interview/Question_3.py
+++ Question Interview/Question_3.py
@@ -1,1 +1,16 @@
 # 3. Write a sorting function without using the list.sort function
+# from os import remove
+# from numpy import minimum
+
+
+initial_list = [1,2,3,4,-3,4,7,8,9,-6,-2]
+new_list = []
+while initial_list:
+    minimum = initial_list[0]
+    for i in initial_list:
+        if minimum > i: 
+            minimum = i 
+
+    new_list.append(minimum)
+    initial_list.remove(minimum)
+print(new_list)
