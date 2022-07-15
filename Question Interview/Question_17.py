ae0e67d8668c7581a6ba94062697fd76bccc56fa
Tontan-Hak <tontanhak97@gmail.com>
2022-07-15 15:19:06 +0700

    Add answer 17

---
+++ Question Interview/Question_17.py
@@ -0,0 +1,14 @@
+# 17. Write a program to check whether an integer number is a Palindrome or not
+number = int(input("Enter the integer number: "))
+temp = number
+reverse_number = 0
+while(number>0):
+    remainder = number % 10
+    reverse_number = (reverse_number * 10) + remainder
+    number = number // 10 
+print("The reverse of integer number: ", reverse_number)
+
+if (reverse_number==temp):
+    print("True")
+else: 
+    print("False")
