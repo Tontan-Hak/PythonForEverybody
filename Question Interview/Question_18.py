cf49c12c4b7b748ff2e77d3f1b7964cdb57e00c2
Tontan-Hak <tontanhak97@gmail.com>
2022-07-15 15:49:28 +0700

    Answer to question number 18

--- Question Interview/Question_18.py
+++ Question Interview/Question_18.py
@@ -1,1 +1,12 @@
 # 18. Write a program to check number reepresentation is in binary
+
+number = int(input("Enter the integer number: "))
+#temp = number
+while number != 0:
+    remainder = number % 10
+    if(remainder !=0 and remainder !=1):
+        print('Number is not in binary representation!')
+        break
+    number = number // 10
+    if (number==0):
+        print("Number is in binary represesntation!")
