d4ac89133be4e09e2a5b1b6cad86a6cbda86c457
Tontan-Hak <tontanhak97@gmail.com>
2022-07-15 14:58:14 +0700

    Add question 17

--- Question Interview/Question_16.py
+++ Question Interview/Question_16.py
@@ -1,11 +1,12 @@
 # 16 Write a program to reverse an integer number
+
 number = int(input("Enter the integer number: "))
-revs_number = 0
+reverse_number = 0
 while(number>0):
-    R = number % 10
-    revs_number = (revs_number * 10) + R
+    remainder = number % 10
+    reverse_number = (reverse_number * 10) + remainder
     number = number // 10 
-print(revs_number)
+print("The reverse of integer number: ", reverse_number)
 
 
 
--- README.md
+++ README.md
@@ -16,3 +16,4 @@
 14. Write a python program to split strings using newline delimiter 
 15. Given a string as your imput, delete any reoccurring character, and return the new string.
 16. Write a program to reverse an integer number
+17. Write a program to check whether an integer number is a Palindrome or not
