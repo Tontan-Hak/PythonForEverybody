936c6b599893b1af5db958fed67b68fd4ab77740
Tontan-Hak <tontanhak97@gmail.com>
2022-07-15 16:36:50 +0700

    add another question in question 17

--- Question Interview/Question_17.py
+++ Question Interview/Question_17.py
@@ -12,3 +12,15 @@ if (reverse_number==temp):
     print("True")
 else: 
     print("False")
+# def isPalindrome(s):
+#     return s == s[::-1]
+ 
+ 
+# # Driver code
+# s = "malayalam"
+# ans = isPalindrome(s)
+ 
+# if ans:
+#     print("Yes")
+# else:
+#     print("No")
