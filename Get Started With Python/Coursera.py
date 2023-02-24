8d014b5effb0b88a79fbfac9feb76e369882927e
Tontan-Hak <tontanhak97@gmail.com>
2023-02-24 17:58:45 +0700

    update code

---
+++ Get Started With Python/Coursera.py
@@ -0,0 +1,8 @@
+# Dicision structure
+temperature = float(input("Enter the temperature: "))
+
+if temperature <= 10:
+	print("Beeps", "Beeps", "Beeps")	
+	print("Turn on the heater!")
+	
+print("Please check again in an hour.")
