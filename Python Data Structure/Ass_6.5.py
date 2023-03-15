36c147a60878fe32756e6c12351aa37e4075ffcf
Tontan-Hak <tontanhak97@gmail.com>
2023-03-15 22:37:48 +0700

    add code ass 6.5

--- Python Data Structure/Ass_6.5.py
+++ Python Data Structure/Ass_6.5.py
@@ -1,5 +1,18 @@
-# 
+# Assignmnet 6.5
 text = "X-DSPAM-Confidence:    0.8475";
 text =text.replace("","")
 ind=text.find(":")
 print(float(text[ind+1:]))
+# other way
+text = "X-DSPAM-Confidence:    0.8475";
+
+spacePos = text.find(" ")
+number = text[spacePos::1]
+#not really necessary but since we are just learning and playing
+strippedNumber = number.lstrip();
+result = float(strippedNumber)
+
+def reprint(printed):
+    print(printed) 
+
+reprint(result)
