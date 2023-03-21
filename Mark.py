dd56a0d76ecb3f51818a49a77c43e1af946edcea
Tontan-Hak <tontanhak97@gmail.com>
2023-03-21 09:43:13 +0700

    Control structure

--- Mark.py
+++ Mark.py
@@ -13,5 +13,16 @@ elif mark >= 60:
 elif mark >= 50:
     	print("Congratulations, you have scored a D!")
 else:
-    print("You have scored an N!")
+    print("You have scored an F!")
     print("Unfortunately, you did not pass!")
+
+# Vote
+citizen = input("Are you a citizen of Australia (y/n)? ")
+if citizen == 'n':
+    	print("You are not eligible to vote.")
+else:
+    age = int(input("How old are you (on your last birthday)? "))
+if age < 18:
+    print("You are too young to vote")
+else:
+	print("You are eligible to vote")
