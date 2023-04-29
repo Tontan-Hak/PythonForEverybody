148f6ed32184745f8efb6bbc5b0b5ebfff16102f
Tontan-Hak <tontanhak97@gmail.com>
2023-04-29 09:31:36 +0700

    Add ass 9.4

---
+++ Python Data Structure/Ass_9.4.py
@@ -0,0 +1,19 @@
+#Use mbox-short.txt as File name
+name = input("Enter file:")
+tekst = open(name)
+dic = {}
+
+for lines in tekst:
+    if lines.startswith("From "):
+        words = lines.split()
+        email = words[1]
+        dic[email] = dic.get(email, 0)+1
+                   
+i = None
+j = None
+
+for k, v in dic.items():
+    if j is None or j < v:
+        j = v
+        i = k
+print(i, j)
