67445dada332bd0e5d6d04f0f5ea300ba09f55f3
Tontan-Hak <tontanhak97@gmail.com>
2023-05-09 13:39:30 +0700

    Edit code of exception

--- Using Python to Access Web Data/Exception.py
+++ Using Python to Access Web Data/Exception.py
@@ -1,4 +1,19 @@
 # Example of exception
-T = int(input("Enter an integer: "))
-print("The number you enter is ", T)
-print("Bye!")
+# T = int(input("Enter an integer: "))
+# print("The number you enter is ", T)
+#print("Bye!")
+
+# 
+def main():
+    try:
+        weight = int(input("Enter the weight in kg: " ))   
+        heightInCM = int(input("Enter your height in cm: "))
+        bmi = calculateBMI(weight, heightInCM);
+        print("BMI: {:.2f}".format(bmi))
+        ...
+
+    except:
+        print("Some input is in wrong format or outside valid range!")
+        print("Please try again.")
+
+main()
