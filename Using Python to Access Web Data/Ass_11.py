773e951157a5dfde4700ccc5d744e47e14440317
Tontan-Hak <tontanhak97@gmail.com>
2023-05-10 10:21:16 +0700

    add new ass 11 code

---
+++ Using Python to Access Web Data/Ass_11.py
@@ -0,0 +1,16 @@
+# Ass chapter 11
+import re
+
+if __name__ == '__main__':
+    file = open('regex_sum_724424.txt')
+    sm = 0 
+    wd = 0 
+    for line in file:
+        temp = line.rstrip()
+        temp = re.findall('[0-9]+', temp)
+        if len(temp) > 0:
+            for w in temp:
+                wd += 1
+                sm += int(w)
+
+    print('The sum for the sample text above is %d\n' % sm)
