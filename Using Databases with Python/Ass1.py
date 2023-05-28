922776386555a116b7547ab6fbb2f44d500de7c4
Tontan-Hak <tontanhak97@gmail.com>
2023-05-29 01:17:22 +0700

    update code

--- Using Databases with Python/Ass1.py
+++ Using Databases with Python/Ass1.py
@@ -1,7 +1,6 @@
-# Data Base
 import sqlite3
 
-conn = sqlite3.connect('emaildb.sqlite')
+conn = sqlite3.connect('Counts.sqlite')
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Counts')
@@ -10,21 +9,24 @@ cur.execute('''
 CREATE TABLE Counts (org TEXT, count INTEGER)''')
 
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'mbox-short.txt'
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
-    (emailname, organization) = email.split('@')
-    cur.execute('SELECT count FROM Counts WHERE org = ? ', (organization,))
+    email = line.split("@")
+    email = email[1]
+    email = email.split("\n")
+    email = email[0]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (email,))
     row = cur.fetchone()
     if row is None:
         cur.execute('''INSERT INTO Counts (org, count)
-                VALUES (?, 1)''', (organization,))
+                VALUES (?, 1)''', (email,))
     else:
         cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
-                    (organization,))
+                    (email,))
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
