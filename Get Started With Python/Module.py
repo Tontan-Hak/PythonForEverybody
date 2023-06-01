b54ff558570a91e448b94169a484f483774cb3c8
Tontan-Hak <tontanhak97@gmail.com>
2023-06-01 16:45:33 +0700

    add review code for python for everybody

--- Get Started With Python/Module.py
+++ Get Started With Python/Module.py
@@ -16,3 +16,21 @@ if __name__=="__main__":
 
 # Note that when we inport module conversion, the main function is called and executed
 # We can suppress this call when we import the module by putting the call inside a special if statement 
+
+x = 12
+if x < 5:
+    print("smaller")
+else:
+    print("bigger")
+print("all done")
+
+# 
+stuff = ['joseph', 'sally', 'walter', 'tim']
+print(stuff[2])
+# 
+def hello():
+    print("Hello")
+    print("There")
+
+x = 10
+x = x + 1
---
+++ Get Started With Python/Review.py
@@ -0,0 +1,27 @@
+# 
+def hello():
+    print("Hello")
+    print("There")
+
+x = 10
+x = x + 1
+# 
+x = -1
+for value in [3, 41, 12, 9, 74, 15] :
+    if value > x :
+        x = value
+print(x)
+# 
+total = 0
+for abc in range(5):
+    total = total + abc
+print(total)
+# 
+line1
+try:
+    line2
+    line3
+    line4
+except:
+    line5
+    line6
